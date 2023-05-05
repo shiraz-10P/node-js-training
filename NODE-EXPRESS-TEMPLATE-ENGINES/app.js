25674c5f84b5505c12f4f712eff70e39d53eb174
shiraz-10P <shiraz.jafri@10pearls.com>
2023-05-05 18:49:09 +0500

    Welcome ejs

--- NODE-EXPRESS-TEMPLATE-ENGINES/app.js
+++ NODE-EXPRESS-TEMPLATE-ENGINES/app.js
@@ -4,16 +4,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHandlebars = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHandlebars({
-    layoutsDir: 'views/layouts',
-    defaultLayout: 'main-layout',
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
